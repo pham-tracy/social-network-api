c205ab85302508d4324c1951c0d4ce23ea4903c9
Tracy Pham <phamtracy95@gmail.com>
2022-12-20 20:33:32 -0800

    Add thought Routes and controller

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+// /api/thoughts
+router
+  .route("/")
+  .get(getThoughts)
+  .get(getSingleThought)
+  .post(createThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+  .route("/:thoughtId/reactions")
+  .post(createReaction)
+  .delete(removeReaction);
+
+module.exports = router;

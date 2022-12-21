e55b9a62b98de280a89a16a6e9e660f4904782f2
Tracy Pham <phamtracy95@gmail.com>
2022-12-20 19:22:49 -0800

    Add User.js Model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, Types } = require("mongoose");
+const Thought = require("./Thought");
+const User = require("../models");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      //TODO: validate email with Mongoose' matching validation
+    },
+    thoughts: [
+      {
+        // Array of _id values referencing the Thought model
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        // Array of _id values referencing the User model (self-reference)
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual that retrieves length of user's friends array field on query
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+module.exports = userSchema;
